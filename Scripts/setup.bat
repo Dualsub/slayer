4e905b6259b06855a69acf1877d6af64255ef671
Simon Sjöling <rizedualsub@gmail.com>
2023-11-08 13:58:17 +0100

    Added setup script

---
+++ Scripts/setup.bat
@@ -0,0 +1,17 @@
+@REM Building the project
+echo Building the project
+mkdir build
+cd build
+call cmake ..
+call cmake --build . --config RelWithDebInfo
+cd ..
+
+@REM Building asset pack
+echo Building asset pack
+cd Tools
+python -m virtualenv venv
+call venv\Scripts\activate.bat
+pip install -r requirements.txt
+call buildbat.bat
+deactivate
+cd ..
